fd73648cdb094fad7f24909570e7f98e219da3eb
txectn <nahid32277@gmail.com>
2026-07-28 07:28:57 +0600

    Set up frontend assets and global styles

---
+++ frontend/src/assets/icons/icons.jsx
@@ -0,0 +1,99 @@
+export function UserIcon({ width = "21", height = "21", stroke = "black", strokeWidth = "1.7" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.2568 9.23813C12.5646 9.23813 14.4354 7.36074 14.4354 5.04485C14.4354 2.72896 12.5646 0.851562 10.2568 0.851562C7.94897 0.851562 6.07812 2.72896 6.07812 5.04485C6.07812 7.36074 7.94897 9.23813 10.2568 9.23813Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M0.851562 19.599C0.851563 17.0967 1.84212 14.6969 3.60533 12.9275C5.36854 11.1581 7.75996 10.1641 10.2535 10.1641C12.7471 10.1641 15.1385 11.1581 16.9017 12.9275C18.6649 14.6969 19.6555 17.0967 19.6555 19.599H0.851562Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function UserIcon2({ width = "21", height = "21", stroke = "black", strokeWidth = "1.5" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.125 10.1641H8.042C7.08438 10.164 6.13613 10.3526 5.25139 10.719C4.36666 11.0854 3.56277 11.6226 2.88563 12.2997C2.20849 12.9768 1.67137 13.7807 1.30494 14.6655C0.938502 15.5502 0.749934 16.4984 0.75 17.4561C0.75 17.5929 0.776956 17.7283 0.829327 17.8547C0.881699 17.9811 0.95846 18.096 1.05522 18.1927C1.15199 18.2894 1.26686 18.3661 1.39328 18.4184C1.5197 18.4707 1.65519 18.4976 1.792 18.4976H16.375C16.5118 18.4976 16.6473 18.4707 16.7737 18.4184C16.9001 18.3661 17.015 18.2894 17.1118 18.1927C17.2085 18.096 17.2853 17.9811 17.3377 17.8547C17.39 17.7283 17.417 17.5929 17.417 17.4561C17.4171 16.4984 17.2285 15.5502 16.8621 14.6655C16.4956 13.7807 15.9585 12.9768 15.2814 12.2997C14.6042 11.6226 13.8003 11.0854 12.9156 10.719C12.0309 10.3526 11.0826 10.164 10.125 10.1641Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M9.0852 9.17C11.4104 9.17 13.2954 7.28537 13.2954 4.9602C13.2954 2.63503 11.4104 0.75 9.0852 0.75C6.76003 0.75 4.875 2.63503 4.875 4.9602C4.875 7.28537 6.76003 9.17 9.0852 9.17Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+
+export function CartIcon({width = "19px", height = "21px", stroke = "black", strokeWidth = "1.8"}) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 19 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.0859 8.39844C13.0859 9.393 12.6908 10.3468 11.9876 11.0501C11.2843 11.7533 10.3305 12.1484 9.33594 12.1484C8.34138 12.1484 7.38755 11.7533 6.68429 11.0501C5.98103 10.3468 5.58594 9.393 5.58594 8.39844M0.995 4.68031H17.6769M1.27344 4.14875C1.03002 4.47331 0.898438 4.86806 0.898438 5.27375V17.7734C0.898438 18.2707 1.09598 18.7476 1.44761 19.0993C1.79924 19.4509 2.27616 19.6484 2.77344 19.6484H15.8984C16.3957 19.6484 16.8726 19.4509 17.2243 19.0993C17.5759 18.7476 17.7734 18.2707 17.7734 17.7734V5.27375C17.7734 4.86806 17.6419 4.47331 17.3984 4.14875L15.5234 1.64844C15.3488 1.41557 15.1223 1.22656 14.862 1.09639C14.6016 0.96621 14.3145 0.898437 14.0234 0.898438H4.64844C4.35735 0.898437 4.07027 0.96621 3.80991 1.09639C3.54956 1.22656 3.32309 1.41557 3.14844 1.64844L1.27344 4.14875Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function HeartIcon({ width = "22", height = "20", stroke = "black", strokeWidth = "2" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 6.51615C1.00002 5.40335 1.33759 4.31674 1.96813 3.39982C2.59867 2.4829 3.49252 1.77881 4.53161 1.38055C5.5707 0.982294 6.70616 0.908598 7.78801 1.1692C8.86987 1.4298 9.84722 2.01243 10.591 2.84015C10.6434 2.89617 10.7067 2.94082 10.7771 2.97135C10.8474 3.00188 10.9233 3.01764 11 3.01764C11.0767 3.01764 11.1526 3.00188 11.2229 2.97135C11.2933 2.94082 11.3566 2.89617 11.409 2.84015C12.1504 2.00705 13.128 1.41952 14.2116 1.15575C15.2952 0.891989 16.4335 0.9645 17.4749 1.36364C18.5163 1.76277 19.4114 2.46961 20.0411 3.39006C20.6708 4.3105 21.0053 5.40091 21 6.51615C21 8.80615 19.5 10.5162 18 12.0162L12.508 17.3292C12.3217 17.5432 12.0919 17.7151 11.834 17.8335C11.5762 17.9518 11.296 18.014 11.0123 18.0158C10.7285 18.0176 10.4476 17.959 10.1883 17.8439C9.92893 17.7288 9.69703 17.5598 9.508 17.3482L4 12.0162C2.5 10.5162 1 8.81615 1 6.51615Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function MenuIcon({ width = "20", height = "16", stroke = "black", strokeWidth = "2" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1H19M1 8H19M1 15H19" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function MenuIcon2({ width = "18", height = "16", stroke = "black", strokeWidth = "2" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1H17M1 8H17M1 15H17" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+
+export function SearchIcon({ width = "20", height = "20", stroke = "black", strokeWidth = "2" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 19L14.66 14.66M17 9C17 13.4183 13.4183 17 9 17C4.58172 17 1 13.4183 1 9C1 4.58172 4.58172 1 9 1C13.4183 1 17 4.58172 17 9Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function HomeIcon({ width = "19", height = "20", stroke = "black", strokeWidth = "1.8" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.8984 19.8989V11.8989C12.8984 11.6337 12.7931 11.3793 12.6055 11.1918C12.418 11.0043 12.1637 10.8989 11.8984 10.8989H7.89844C7.63322 10.8989 7.37887 11.0043 7.19133 11.1918C7.00379 11.3793 6.89844 11.6337 6.89844 11.8989V19.8989M0.898438 8.89892C0.898368 8.60798 0.961771 8.32054 1.08423 8.05663C1.20668 7.79273 1.38524 7.55871 1.60744 7.37092L8.60744 1.37092C8.96842 1.06583 9.42579 0.898438 9.89844 0.898438C10.3711 0.898438 10.8285 1.06583 11.1894 1.37092L18.1894 7.37092C18.4116 7.55871 18.5902 7.79273 18.7127 8.05663C18.8351 8.32054 18.8985 8.60798 18.8984 8.89892V17.8989C18.8984 18.4293 18.6877 18.9381 18.3127 19.3131C17.9376 19.6882 17.4289 19.8989 16.8984 19.8989H2.89844C2.368 19.8989 1.8593 19.6882 1.48422 19.3131C1.10915 18.9381 0.898438 18.4293 0.898438 17.8989V8.89892Z" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function CloseIcon({ width = "14", height = "14", stroke = "black", strokeWidth = "2" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13 1L1 13M1 1L13 13" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function ArrowIcon({ width = "14", height = "8", stroke = "black", strokeWidth = "2" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L7 7L13 1" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function ArrowIcon2({ width = "18", height = "14", stroke = "black", strokeWidth = "1.5" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.75 6.75H16.75M10.75 0.75L16.75 6.75L10.75 12.75" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export function ArrowIcon3({ width = "15", height = "16", stroke = "black", strokeWidth = "1.7" }) {
+    return (
+        <svg width={width} height={height} viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.35156 14.8516V0.851562M13.8516 7.85156L7.35156 0.851562L0.851562 7.85156" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
